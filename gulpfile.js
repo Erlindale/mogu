05caf4283d6c470ec59324f88961610878ec4549
Erlindale <903711370@qq.com>
2018-01-19 09:41:55 +0800

    蘑菇街主线1.18提交

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,29 @@ const gulp = require("gulp");
 const sass = require("gulp-sass-china");
 const babel = require("gulp-babel");
 const connect = require("gulp-connect");
-
+const images = require("gulp-imagemin");
 gulp.task("html",()=>{
 	return gulp
 				.src(["*.html"])
 				.pipe(gulp.dest("dist"))
 				.pipe(connect.reload());
+
 })
+gulp.task("shtml",()=>{
+	return gulp.src(["html/*.html"])
+				.pipe(gulp.dest("dist/html"))
+				.pipe(connect.reload());
+})
+gulp.task("images",()=>{
+	return gulp
+				.src(["images/**/*"])
+				.pipe(gulp.dest("dist/images"))
+				.pipe(connect.reload());
+
+})
+
 gulp.task("watch",()=>{
-	gulp.watch(["scss/*.scss","*.html"],["sass","html"]);
+	gulp.watch(["scss/*.scss","*.html","images/**/*","html/*.html"],["sass","html","images","shtml"]);
 })
 gulp.task("server",function(){
 	connect.server({
